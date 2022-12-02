72d85b6ed91033e5f128ed51e98728b854bae09e
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2022-12-01 17:31:20 -0700

    instructions

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,114 @@ function setup() {
   aimingGameCtx = new Context('transparent', 'static/img/aimgame/background.jpg')
   typingGameCtx = new Context('transparent', 'static/img/typegame/background.jpg')
   matchingGameCtx = new Context('transparent', 'static/img/matchgame/background.jpg')
+  aimingGameInstructionCtx = new Context('transparent', 'static/img/aimgame/background.jpg')
+  typingGameInstructionCtx = new Context('transparent', 'static/img/typegame/background.jpg')
+  matchingGameInstructionCtx = new Context('transparent', 'static/img/matchgame/background.jpg')
+  aimingGameInstructionCtx.addElements([
+    new Title(aimingGameInstructionCtx, 'Aiming', {
+      'font-size': '4em',
+      'color': '#d9a362',
+      'text-align': 'center',
+      'font-weight': 'bold',
+      'font-family': 'Futura',
+      'z-index': '100'
+    }, 0, windowHeight * 0.015),
+    new Button(aimingGameInstructionCtx, '< Back', switchContext, [aimingGameCtx, aimingGameInstructionCtx], {
+      'font-size': '2em',
+      'color': '#d9a362',
+      'border-radius': '2em',
+      'border': '0.1em #d9a362 solid',
+      'padding': '0.2em 1em',
+      'width': '4em',
+      'text-align': 'center',
+      'font-family': 'Futura',
+      'z-index': '100'
+    }, windowWidth * 0.05, windowHeight * 0.025),
+    new DecorativeBox(aimingGameInstructionCtx, '/static/img/aimgame/wood.jpg', {
+      'width': '100vw',
+      'height': '12vh',
+    }, 0, 0),
+    new TextBox(aimingGameInstructionCtx, 'After clicking start, hit as many ducks as you can within 30 seconds. Try not to miss, or your score will take a hit!', {
+      'font-size': '2.5em',
+      'border-radius': '2em',
+      'color': 'black',
+      'border': '0.1em black solid',
+      'padding': '0.4em 1em',
+      'text-align': 'center',
+      'font-family': 'Monaco',
+      'overflow': 'hidden',
+      'width': '90vw',
+      'box-sizing': 'border-box',
+      'background': 'white',
+    }, windowWidth * 0.05, windowHeight * 0.4),
+  ])
+  typingGameInstructionCtx.addElements([
+    new Title(typingGameInstructionCtx, 'Typing', {
+      'font-size': '4em',
+      'color': 'black',
+      'text-align': 'center',
+      'font-weight': 'bold',
+      'font-family': 'Futura',
+      'z-index': '100'
+    }, 0, windowHeight * 0.015),
+    new Button(typingGameInstructionCtx, '< Back', switchContext, [typingGameCtx, typingGameInstructionCtx], {
+      'font-size': '2em',
+      'border-radius': '2em',
+      'border': '0.1em black solid',
+      'padding': '0.2em 1em',
+      'width': '4em',
+      'text-align': 'center',
+      'font-family': 'Futura',
+      'background': 'rgba(255,255,255,0.75)',
+      'z-index': '100'
+    }, windowWidth * 0.05, windowHeight * 0.025),
+    new TextBox(typingGameInstructionCtx, 'After clicking start, use your keyboard to type the entire paragraph as fast as possible. You make any mistakes and you will not be able to continue, so make sure to correct ALL your mistakes.', {
+      'font-size': '2.5em',
+      'border-radius': '2em',
+      'color': 'black',
+      'border': '0.1em black solid',
+      'padding': '0.4em 1em',
+      'text-align': 'center',
+      'font-family': 'Monaco',
+      'overflow': 'hidden',
+      'width': '90vw',
+      'box-sizing': 'border-box',
+      'background': 'white',
+    }, windowWidth * 0.05, windowHeight * 0.4),
+  ])
+  matchingGameInstructionCtx.addElements([
+    new Title(matchingGameInstructionCtx, 'Matching', {
+      'font-size': '4em',
+      'color': 'black',
+      'text-align': 'center',
+      'font-weight': 'bold',
+      'font-family': 'Futura',
+      'z-index': '100'
+    }, 0, windowHeight * 0.015),
+    new Button(matchingGameInstructionCtx, '< Back', switchContext, [matchingGameCtx, matchingGameInstructionCtx], {
+      'font-size': '2em',
+      'border-radius': '2em',
+      'border': '0.1em black solid',
+      'padding': '0.2em 1em',
+      'width': '4em',
+      'text-align': 'center',
+      'font-family': 'Futura',
+      'z-index': '100'
+    }, windowWidth * 0.05, windowHeight * 0.025),
+    new TextBox(matchingGameInstructionCtx, 'After clicking start, match the animal cards together by clicking on them. Try to match all the cards in the fastest time and fewest attempts!', {
+      'font-size': '2.5em',
+      'border-radius': '2em',
+      'color': 'black',
+      'border': '0.1em black solid',
+      'padding': '0.4em 1em',
+      'text-align': 'center',
+      'font-family': 'Monaco',
+      'overflow': 'hidden',
+      'width': '90vw',
+      'box-sizing': 'border-box',
+      'background': 'white',
+    }, windowWidth * 0.05, windowHeight * 0.4),
+  ])
   i1 = createImg('static/img/aiming-icon.png', 'picture')
   i1.style('max-width', '100%')
   i1.style('object-fit', 'contain')
@@ -148,7 +256,18 @@ function setup() {
         'width': '4em',
         'text-align': 'center',
         'font-family': 'Futura'
-      }, windowWidth * 0.8, windowHeight * 0.9, null, aimingGame)
+      }, windowWidth * 0.8, windowHeight * 0.9, null, aimingGame),
+      new Button(aimingGameCtx, '?', switchContext, [aimingGameInstructionCtx, aimingGameCtx], {
+        'font-size': '2em',
+        'color': '#d9a362',
+        'border-radius': '2em',
+        'border': '0.1em #d9a362 solid',
+        'padding': '0.2em 1em',
+        'width': '1em',
+        'text-align': 'center',
+        'font-family': 'Futura',
+        'z-index': '100'
+      }, windowWidth * 0.2, windowHeight * 0.025)
     ]
   )
   typingGame = new TypeGame(typingGameCtx)
@@ -231,7 +350,17 @@ function setup() {
         'overflow': 'hidden',
         'white-space': 'nowrap',
         'background': 'rgba(255,255,255,0.75)'
-      }, windowWidth * 0.05, windowHeight * 0.9)
+      }, windowWidth * 0.05, windowHeight * 0.9),
+      new Button(typingGameCtx, '?', switchContext, [typingGameInstructionCtx, typingGameCtx], {
+        'font-size': '2em',
+        'border-radius': '2em',
+        'border': '0.1em black solid',
+        'padding': '0.2em 1em',
+        'width': '1em',
+        'text-align': 'center',
+        'font-family': 'Futura',
+        'background': 'rgba(255,255,255,0.75)'
+      }, windowWidth * 0.2, windowHeight * 0.025)
     ]
   )
   matchingGame = new MatchGame(matchingGameCtx)
@@ -291,6 +420,15 @@ function setup() {
         'overflow': 'hidden',
         'white-space': 'nowrap',
       }, windowWidth * 0.8, windowHeight * 0.1, '0'),
+      new Button(matchingGameCtx, '?', switchContext, [matchingGameInstructionCtx, matchingGameCtx], {
+        'font-size': '2em',
+        'border-radius': '2em',
+        'border': '0.1em black solid',
+        'padding': '0.2em 1em',
+        'width': '1em',
+        'text-align': 'center',
+        'font-family': 'Futura'
+      }, windowWidth * 0.2, windowHeight * 0.025)
     ]
   )
   matchingGame.preload()
